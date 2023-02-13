d1f483a267b7eece824c4fdc48801d3c611cff5b
YaWK <danielretzl@gmail.com>
2023-02-13 05:30:19 +0100

    issue #7: add comments to jscode

--- src/js/changeCSS.app.js
+++ src/js/changeCSS.app.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     // Handle file upload form submission
     $('#css-upload-form').submit(function(event) {
+        // Prevent default form submission behavior
         event.preventDefault();
+        // Get the form data and create a new FormData object
         var formData = new FormData($(this)[0]);
+        // Send an AJAX request to the process.php script
         $.ajax({
             url: 'src/classes/process.php',
             type: 'POST',
@@ -10,19 +13,25 @@ $(document).ready(function() {
             contentType: false,
             processData: false,
             success: function(response) {
+                // Update the CSS editor with the response HTML
                 $('#css-editor').html(response);
             },
             error: function(xhr, status, error) {
+                // Handle AJAX errors
                 console.log('Error: ' + error);
             }
         });
     });
 
-// Handle update form submission
+    // Handle update form submission
     $('#css-editor').on('submit', 'form#css-update-form', function(event) {
+        // Prevent default form submission behavior
         event.preventDefault();
+        // Create a new FormData object from the form
         var formData = new FormData(this);
+        // Add the filename input to the form data
         formData.append('filename', $('input[name="filename"]').val());
+        // Send an AJAX request to the update.php script
         $.ajax({
             url: 'src/classes/update.php',
             type: 'POST',
@@ -30,16 +39,20 @@ $(document).ready(function() {
             processData: false,
             contentType: false,
             success: function(response) {
+                // Create a download link for the modified CSS file
                 var a = document.createElement('a');
                 var url = window.URL.createObjectURL(new Blob([response]));
                 a.href = url;
                 a.download = $('input[name="filename"]').val();
+                // Add the link to the page and trigger the download
                 $('body').append(a);
                 a.click();
+                // Clean up the link and URL object
                 window.URL.revokeObjectURL(url);
                 $(a).remove();
             },
             error: function(xhr, status, error) {
+                // Handle AJAX errors
                 console.log('Error: ' + error);
             }
         });
