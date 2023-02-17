fc73c7a3063d09304e6766de78cdae83c136b1d6
YaWK <danielretzl@gmail.com>
2023-02-17 03:17:34 +0100

    issue #7: add slide behavior to frontpage

--- src/js/changeCSS.app.js
+++ src/js/changeCSS.app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function (keyframes, options) {
     // Handle file upload form submission
     $('#css-upload-form').submit(function(event) {
         // Prevent default form submission behavior
@@ -18,11 +18,8 @@ $(document).ready(function() {
             success: function(response)
             {   // Some Animations
                 // hide the upload form, logo and intro text; show the css editor
-                $('#firstLine').fadeOut(200);
-                $('#introduction').fadeOut(200);
-                $('#logo').slideUp(600)
-                $('#leadText').slideUp(680);
-                $('#css-upload-form').slideUp(600);
+                $('#introduction').hide();
+                $('#jumbotron').slideUp(800);
                 // insert the xhr response into the css editor
                 $('#css-editor').html(response).fadeIn(800);
                 // because of xhr we need to select all fields with jscolor data attribute
@@ -92,6 +89,101 @@ $(document).ready(function() {
             $(tabId).html('<p>Loading... <b>'+group+'</b></p>'); // Loading indicator
         });
     });
+
+    $(document).ready(function() {
+        $('#jumbotron').css('visibility', 'visible');
+        $('#navbar').css('visibility', 'visible');
+    });
+
+    //
+    // $(document).ready(function() {
+    //     animateObjects();
+    // });
+    //
+    // function setRandomColors(numElements) {
+    //     for (var i = 1; i <= numElements; i++) {
+    //         var hexCode = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    //         $('#colorObject' + i).text(hexCode);
+    //     }
+    // }
+    //
+    // setRandomColors(5);
+    //
+    // function animateObjects() {
+    //     $('.object').each(function() {
+    //         var object = $(this); // Store $(this) reference in a variable
+    //         var objectWidth = object.outerWidth();
+    //         var objectHeight = object.outerHeight();
+    //         var windowWidth = $(window).width();
+    //         var windowHeight = $(window).height();
+    //
+    //         // set initial position at top of viewport
+    //         var startX = Math.floor(Math.random() * (windowWidth - objectWidth));
+    //         var startY = -objectHeight;
+    //
+    //         // generate random x value
+    //         var xDirection = Math.random() < 0.5 ? -1 : 1; // randomly select left or right direction
+    //         var xDistance = Math.floor(Math.random() * (windowWidth - objectWidth)); // randomly select distance from left edge
+    //         var x = xDirection * xDistance;
+    //
+    //         // generate random y value
+    //         var y = Math.floor(Math.random() * ($(window).height() - objectHeight));
+    //
+    //         var duration = Math.floor(Math.random() * 20) + 20;
+    //         var startTime = performance.now();
+    //         var lastTime = startTime;
+    //         var currentTime;
+    //
+    //         // use requestAnimationFrame to schedule animation
+    //         function step() {
+    //             currentTime = performance.now();
+    //             var elapsed = currentTime - lastTime;
+    //             var progress = Math.min(elapsed / (duration * 1000), 1);
+    //
+    //             var position = object.position(); // Use stored object variable instead of $(this)
+    //             var newPosition = {
+    //                 left: position.left + x * progress,
+    //                 top: position.top + y * progress
+    //             };
+    //
+    //             // check if object has reached the middle of the viewport
+    //             if (newPosition.top >= windowHeight / 1.75 - objectHeight / 1.75) {
+    //                 var fadeOutDuration = duration * 500 / 6;
+    //
+    //                 // fade out and re-generate the object after fade out is complete
+    //                 object.fadeOut({
+    //                     duration: fadeOutDuration,
+    //                     queue: false,
+    //                     complete: function() {
+    //                         object.css({
+    //                             left: Math.floor(Math.random() * (windowWidth - objectWidth)),
+    //                             top: -objectHeight
+    //                         }).fadeIn({
+    //                             duration: fadeOutDuration,
+    //                             queue: false
+    //                         });
+    //                     }
+    //                 });
+    //             } else {
+    //                 object.css(newPosition); // Use stored object variable instead of $(this)
+    //                 lastTime = currentTime;
+    //                 requestAnimationFrame(step);
+    //             }
+    //         }
+    //
+    //         object.css({
+    //             left: startX,
+    //             top: startY
+    //         }).fadeIn({
+    //             duration: duration * 1000 / 2,
+    //             queue: false
+    //         });
+    //
+    //         requestAnimationFrame(step);
+    //     });
+    // }
+
+
 
 
     // // Handle update form submission (work in progress!)
