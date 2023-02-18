d4c2fed987cb6a77cfb0895c67626cc28446caca
YaWK <danielretzl@gmail.com>
2023-02-19 00:08:04 +0100

    issue #14: fixed array typo error

--- src/classes/changeCSSApp.class.php
+++ src/classes/changeCSSApp.class.php
@@ -355,15 +355,14 @@ class changeCSSApp
         // Define the mapping array
         $mapping = [
             'Fonts' => [
-                'Fonts' => [
-                    'H1' => ['.h1'],
-                    'H2' => ['.h2'],
-                    'H3' => ['.h3'],
-                    'H4' => ['.h4'],
-                    'H5' => ['.h5'],
-                    'H6' => ['.h6'],
-                    'Paragraph' => ['.p'],
-                    'Link' => ['.a'],
+                'H1' => ['.h1'],
+                'H2' => ['.h2'],
+                'H3' => ['.h3'],
+                'H4' => ['.h4'],
+                'H5' => ['.h5'],
+                'H6' => ['.h6'],
+                'Paragraph' => ['.p'],
+                'Link' => ['.a'],
             ],
             'Navigation' => [
                 'Navigation' => ['.nav'],
